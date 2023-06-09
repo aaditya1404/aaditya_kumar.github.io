6c92eda36d1e97aab33e5ef2cafb1a96058baf79
Aaditya Kumar Vishwakarma <aadityakumarvishwakarma31@gmail.com>
2023-06-09 15:41:49 +0530

    20th commit

--- reveal.js
+++ reveal.js
@@ -1,3 +1,18 @@
 // ScrollReveal().reveal(".skills-logo");
 // ScrollReveal().reveal('.skills-logo', { easing: 'ease-in' });
 // ScrollReveal().reveal('.skills-logo', { easing: 'steps(5)' });
+
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if(entry.isIntersecting){
+//             entry.target.classList.add('show');
+//         } else {
+//             entry.target.classList.remove('show');
+//         }
+//     });
+// });
+
+// const hiddenElements = document.querySelectorAll('.hidden');
+// hiddenElements.forEach((el) => observer.observe(el));
